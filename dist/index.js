41d0e287cb3edc5671bd91c6bbe64f7da2093700
Daniel Rickert <daniel.rickert@student.uni-luebeck.de>
2017-06-02 11:34:03 +0200

    Removed ./dist from SCM

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-exports.__esModule = true;
-// Sample exports from other modules
-__export(require("./annotation/get-logger"));
-// Sample export of our own
-exports.bas = "123";
