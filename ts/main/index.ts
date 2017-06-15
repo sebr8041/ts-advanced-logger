f0b7347535cd4dd2dc6ec068b1575cafdefdf469
Sebastian Brodersen <brodersen.sebastian@googlemail.com>
2017-06-15 18:20:44 +0200

    changed package.json config and tsconfig. now module works as npm typescript module.

--- ts/main/index.ts
+++ ts/main/index.ts
@@ -1,7 +1,10 @@
 // Sample exports from other modules
-//export * from "./annotation/get-logger";
-//export * from "./service/i-logger";
-//export * from "./service/logger";
+export * from "./annotation/get-logger";
+export * from "./service/i-logger";
+export * from "./service/logger";
+
+//export = Office; // make it a module
+//export as namespace Office; // keep a global namespace called Office
 
 // Sample export of our own
 export var bas = "123";
