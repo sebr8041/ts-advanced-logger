aefcccb9ef6351014cf0b131477ae78315392dc2
Sebastian Brodersen <brodersen.sebastian@googlemail.com>
2017-06-21 12:23:38 +0200

    remove dist and test dist folder

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// Sample exports from other modules
-//export * from "./annotation/get-logger";
-//export * from "./service/i-logger";
-//export * from "./service/logger";
-Object.defineProperty(exports, "__esModule", { value: true });
-// Sample export of our own
-exports.bas = "123";
