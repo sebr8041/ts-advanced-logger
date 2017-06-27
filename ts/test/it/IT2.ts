1df7bd09c32cd78431e0813f49c159b79b1f0dc9
Sebastian Brodersen <brodersen.sebastian@googlemail.com>
2017-06-27 18:50:01 +0200

    add it test cases for logger and loglevelcheker work together

--- ts/test/it/IT2.ts
+++ ts/test/it/IT2.ts
@@ -1,18 +1,46 @@
 import { assert } from "chai";
 import { suite, test } from "mocha-typescript";
-import { ILogger, GetLogger, Logger, LoggerFactory } from "../../main/index"
-
-LoggerFactory.setDefaultLogger(Logger)
+import { ILogger, GetLogger, Logger, LoggerFactory, LogLevelChecker, LogLevelEnum } from "../../main/index"
 
 
+
+/**
+ * mock simulation for console.error.
+ * so check method get called by counting global variable
+ */
+let consoleErrorCallCounter = 0;
+console.error.apply = function (a, b) {
+    consoleErrorCallCounter++;
+}
+
+/**
+ * test suite 2 for testing logger.
+ */
 @suite class IT2Suite {
 
-    @GetLogger()
+    @GetLogger(Logger)
     private logger: ILogger;
 
-
     @test public "Logger logs a message on console"() {
+
+        let currentCounter = consoleErrorCallCounter;
+
         this.logger.error("My Error Message!");
-        assert.isTrue(true)
+        // check console.error get called.
+        assert.isTrue(currentCounter < consoleErrorCallCounter)
+    }
+
+
+    @test public "Logger not logs a message on console because loglevelchecker says no!"() {
+
+        // config log on ERROR global
+        let checker = LogLevelChecker.get();
+        checker.setDefaultLogLevel(LogLevelEnum.FATAL)
+
+        let currentCounter = consoleErrorCallCounter;
+
+        this.logger.error("My Error Message!");
+        // check console.error get NOT called.
+        assert.isTrue(currentCounter === consoleErrorCallCounter)
     }
 }
