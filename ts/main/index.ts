29d0df53be24cb7d0753345bb2298b76c21a19ab
Sebastian Brodersen <brodersen.sebastian@googlemail.com>
2017-07-09 12:11:12 +0200

    add some files to export to lib

--- ts/main/index.ts
+++ ts/main/index.ts
@@ -8,9 +8,16 @@ export * from "./enum/log-level-enum";
 
 // factory
 export * from "./factory/logger-factory";
+export * from "./factory/endpoint-factory";
 
 // rv
 export * from "./rv/rv-method";
+export * from "./rv/i-rv-log";
+export * from "./rv/converter/i-rv-log-converter";
+export * from "./rv/converter/json-rv-log-converter";
+export * from "./rv/endpoint/i-endpoint"
+export * from "./rv/endpoint/ajax-endpoint"
+export * from "./rv/endpoint/buffered-ajax-endpoint"
 
 // services
 export * from "./service/i-logger";
