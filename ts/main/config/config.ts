b459cb5ffb36c611890cda6103e29cd5362c7820
Daniel Rickert <daniel.rickert@student.uni-luebeck.de>
2017-07-26 15:19:15 +0200

    changes config

--- ts/main/config/config.ts
+++ ts/main/config/config.ts
@@ -5,24 +5,26 @@ import { BufferedAjaxEndpoint } from "../rv/endpoint/buffered-ajax-endpoint"
 import { JsonRvLogConverter } from "../rv/converter/json-rv-log-converter"
 import { ILogLevelRule } from "../service/i-log-level-rule"
 
+/**
+ * Interface that provides access to all required configurations
+ */
 export interface ILoggerConfig {
     defaultLoggerClass: ILogger
     bufferSize: number
     serverEnpoint: string
     rvEndpoint: IEndpoint
     rules: ILogLevelRule[]
-
-
 }
 
+/**
+ * DefaultConfiguration that will be used, if no other configuration is defined.
+ */
 export class DefaultLoggerConfig implements ILoggerConfig {
-
 
     defaultLoggerClass: ILogger = new ConsoleLogger();
     bufferSize: number = 5;
     serverEnpoint: string = "http://localhost:8080/logger";
     rules: ILogLevelRule[] = []
-
     rvEndpoint: IEndpoint = new BufferedAjaxEndpoint(this.bufferSize);
 
     constructor() {
@@ -34,11 +36,17 @@ export class DefaultLoggerConfig implements ILoggerConfig {
 export class LoggerConfig {
 
     private static loggerConfig: ILoggerConfig;
-
-    private static setConfig(loggerConfig: ILoggerConfig): void {
+    /**
+     * 
+     * @param loggerConfig The configuration that shall be used by the logger
+     */
+    public static setConfig(loggerConfig: ILoggerConfig): void {
         LoggerConfig.loggerConfig = loggerConfig
     }
 
+    /**
+     * @returns The configuration used by the logger
+     */
     public static getLoggerConfig(): ILoggerConfig {
         if (!LoggerConfig.loggerConfig) {
             LoggerConfig.setConfig(new DefaultLoggerConfig())
